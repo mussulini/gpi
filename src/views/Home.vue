e35d54bfa3b47e355b2467b93f8dcb7197394d9f
Mussulini Lopez <mussulini.l@gmail.com>
2019-01-04 17:33:33 +0800

    WIP: Homepage Section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,165 @@
 <template>
-    <div>
+    <div class="home">
 
-        <banner></banner>
+        <banner id="top"></banner>
 
-        <section class="section section-light">
+        <section id="prometheus" class="section">
+            <header class="header text-center mb-5">
+                <span class="header-sep gradient-primary mb-5"></span>
+                <h1 class="section-header display-2">Prometheus</h1>
+                <h5 class="sub-header">Powerful, Fast &amp; Reliable CMS</h5>
+            </header>
+
+            <div class="container">
+                <div class="row no-gutters">
+                    <div class="col-8">
+                        <h2 class="heading-title">Create your own websites</h2>
+                        <h3 class="heading mb-4">
+                            <span class="header-sep gradient-primary"></span>
+                            <span class="ml-3">More games more revenue</span>
+                        </h3>
+                        <p class="mb-4">Learn more with Gameplay Interactive services, our clients appreciate the fast and hassle-free delivery of their customized white-label iGaming Platform, Reseller and API Integration.</p>
+                        <button class="btn btn-primary">Get Started</button>
+                    </div>
+                    <div class="col-3 illustrations">
+                        <img src="img/home/laptop.png">
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="prometheusFeatures" class="section">
+            <div class="container">
+                <header class="header mb-4">
+                    <h1 class="section-header display-3">CMS Key Features</h1>
+                    <span class="header-sep gradient-primary mt-4"></span>
+                </header>
+
+                <div class="row">
+                    <div class="col-4">
+                        <p><img src="img/home/icon-security.png"></p>
+                        <h5 class="heading">Security</h5>
+                        <p>Users control what content should be accessible and what shouldn’t be to the public. CMS also offers extended measure of security.</p>
+                    </div>
+                    <div class="col-4">
+                        <p><img src="img/home/icon-monitor.png"></p>
+                        <h5 class="heading">24/7 Monitored Portal</h5>
+                        <p>CMS is a centrally monitored portal from where users can upload their content without knowing too many technicalities of uploading.</p>
+                    </div>
+                    <div class="col-4">
+                        <p><img src="img/home/icon-control.png"></p>
+                        <h5 class="heading">Cost Control</h5>
+                        <p>Fewer employees will be needed for IT department (this tips the balance in favor of businesses as it would cut down on the costs.</p>
+                    </div>
+                </div>
+
+                <button class="btn btn-primary mt-5">Learn More</button>
+            </div>
+        </section>
+
+        <section id="products" class="section">
+            <div class="background background-top">
+                <svg height="300" viewBox="0 0 1600 300" width="1600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%">
+                        <stop offset="0" stop-color="#3bd59e" />
+                        <stop offset="1" stop-color="#f7ff02" />
+                    </linearGradient>
+                    <g fill="none" fill-rule="evenodd">
+                        <g fill="#e9ecef">
+                            <path d="m1600 0c-313 0-509.933 196.910631-764.919 205.880731-293.9 10.337541-396.201-53.096013-545.974-43.027575-134.216551 9.021715-201.2886316 45.478331-289.107 72.650377v63.499789h1600z" opacity=".24" transform="matrix(1 0 0 -1 0 299.003322)" />
+                            <path d="m1600 0c-313 0-485.792 201.924917-728.792 224.041196-292.863 26.65515-348.5-54.152492-551.5-58.139203-137.401225-2.698545-217.164392 45.918004-319.708 78.487433v45.141471h1600z" opacity=".48" transform="matrix(1 0 0 -1 0 289.530898)" />
+                            <path d="m1600 .78121634c-366.8-13.60365444-422.958 154.49800666-766.5 189.50132866-270.472 26.790698-381.563-79.013621-627.139-47.631229-55.463153 7.087379-124.2501528 28.008089-206.361 62.76213v33.627571h1600z" transform="matrix(1 0 0 -1 0 239.041018)" />
+                        </g>
+                        <path d="m828.481521 98.8260918c-244.57465-20.6706793-423.472683 109.3363342-828.481521-46.8260918 405.009372 156.121584 583.907236 21.1720546 828.481521 41.8427031 202.481079 17.1126679 508.827819 217.5126149 771.523339 191.6999229-262.69658 25.876477-569.04103-169.603763-771.523339-186.7165342z" fill="url(#a)" />
+                    </g>
+                </svg>
+            </div>
             <div class="container">
                 <header class="header text-center">
-                    <span class="header-sep gradient-primary mb-5"></span>
-                    <h1 class="section-header display-2">Prometheus</h1>
-                    <h5 class="sub-header">Powerful, Fast &amp; Reliable CMS</h5>
+                    <h1 class="section-header display-2">Product Suite</h1>
+                    <h5 class="sub-header">Most Innovative</h5>
+                    <span class="header-sep gradient-primary my-3"></span>
+                    <p class="col-10 offset-1">
+                        It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends; fast to Queequeg's broad canvas belt, and fast to my narrow <a href="">leather one</a>.
+                    </p>
                 </header>
+            </div>
+            <div class="background background-btm">
+                <svg height="240" viewBox="0 0 1600 240" width="1600" xmlns="http://www.w3.org/2000/svg">
+                    <g fill="#e9ecef" fill-rule="evenodd">
+                        <path d="m1600 1c-153.74378 58.3961837-287.82308 135.516295-444.919 141.061162-293.9 10.372-396.201-53.2730005-545.974-43.1710005-247.399 16.6850005-265.399 126.8960005-609.107 99.8960005v41.213838h1600c0-112.321601 0-191.9882673 0-239z" opacity=".24" transform="matrix(-1 0 0 1 1600 0)"/>
+                        <path d="m1600 17c-140.94534 60.5119726-262.79463 138.966832-408.792 152.298855-292.863 26.744-348.5-54.333-551.5-58.333-244.705-4.822-307 153.659258-639.708 93.659258v35.374887h1600c0-113.589117 0-187.9224505 0-223z" opacity=".48" transform="matrix(-1 0 0 1 1600 0)"/>
+                        <path d="m1600.00243 44c-132.39754 49.1354346-235.88231 120.862988-446.50474 142.394736-270.471999 26.88-381.562999-79.277-627.138999-47.79-178.549 22.892-235.316569 131.336-526.35856945 80.67l-.002431 20.725264h1600.00473945c.00008-75.928785.00008-141.2621187 0-196z" transform="matrix(-1 0 0 1 1600.000122 0)"/>
+                    </g>
+                </svg>
             </div>
         </section>
 
         <section class="section">
             <div class="container">
-                <header class="header text-center">
-                    <h1 class="section-header display-3">CMS Key Features</h1>
-                    <span class="header-sep gradient-primary mt-4"></span>
-                </header>
+
+                <div class="row email-request-demo">
+                    <div class="col-5">
+                        <span class="header-sep gradient-primary"></span>
+                        <h1 class="section-header text-white">Get direct access to our products. It’s free!</h1>
+                        <h5 class="text-white">Enter your email address to get you started.</h5>
+                    </div>
+
+                    <div class="col-7">
+                        <div class="input-group my-5">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fa fa-envelope"></i>
+                                </span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="Your e-mail address">
+                            <div class="input-group-append">
+                                <button class="btn btn-neutral" type="button">
+                                    Request Demo
+                                    <span class="fa fa-arrow-right"></span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
 
-        <section class="section section-light">
-            <div class="container">
-                <header class="header text-center">
-                    <h1 class="section-header display-2">Product Suite</h1>
-                    <h5 class="sub-header">Most Innovative</h5>
-                    <span class="header-sep gradient-primary mt-3"></span>
-                </header>
-            </div>
-        </section>
-
-        <section class="section pb-5">
+        <section id="templates" class="section pb-5">
             <div class="container">
                 <header class="header text-center mb-4">
                     <h1 class="section-header">We Craft</h1>
                     <h5 class="sub-header">Get modern & responsive website</h5>
                     <span class="header-sep gradient-primary mt-3"></span>
                 </header>
+
+                <div class="row">
+                    <div class="col-6">
+                        <figure class="figure">
+                            <img src="img/templates/next88-thumb-big.jpg" class="figure-img img-fluid" alt="Next88">
+                            <figcaption class="figure-caption heading">Next88</figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-6">
+                        <figure class="figure">
+                            <img src="img/templates/hot88-thumb-big.jpg" class="figure-img img-fluid" alt="Hot88">
+                            <figcaption class="figure-caption heading">Hot88</figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-6">
+                        <figure class="figure">
+                            <img src="img/templates/alpha88-thumb-big.jpg" class="figure-img img-fluid" alt="Alpha88">
+                            <figcaption class="figure-caption heading">Alpha88</figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-6">
+                        <figure class="figure">
+                            <img src="img/templates/joker88-thumb-big.jpg" class="figure-img img-fluid" alt="Joker88">
+                            <figcaption class="figure-caption heading">Joker88</figcaption>
+                        </figure>
+                    </div>
+                </div>
 
                 <footer class="footer text-center mt-4">
                     <p>We let you test our complete suite of features — with zero risk.</p>
